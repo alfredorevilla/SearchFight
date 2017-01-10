b18fa3d14233a0a01af1ba37855a625735322388
Alfredo Revilla <alfredorevilla@gmail.com>
2017-01-10 15:12:59 -0500

    Added engines first todo implementations.

---
+++ src/ARevillaSearchFight/Engines/BingSearchEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ARevillaSearchFight.Models;
+
+namespace ARevillaSearchFight.Engines
+{
+    [SearchEngineMetadata("Bing")]
+    public class BingSearchEngine : ISearchEngine
+    {
+        public string Name
+        {
+            get { return nameof(BingSearchEngine); }
+        }
+
+        public SearchResults Search(string term)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ARevillaSearchFight/Engines/GoogleCustomSearchEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ARevillaSearchFight.Models;
+
+namespace ARevillaSearchFight.Engines
+{
+    [SearchEngineMetadata("Google")]
+    public class GoogleCustomSearchEngine : ISearchEngine
+    {
+        public string Name
+        {
+            get { return nameof(GoogleCustomSearchEngine); }
+        }
+
+        public SearchResults Search(string term)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
