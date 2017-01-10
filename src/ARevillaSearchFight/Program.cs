2928015e74ee1df2227769ad955772e9eaaab4b9
Alfredo Revilla <alfredorevilla@gmail.com>
2017-01-10 15:15:39 -0500

    Adedd dependency injection support and setup.

--- src/ARevillaSearchFight/Program.cs
+++ src/ARevillaSearchFight/Program.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using ARevillaSearchFight.Views;
+using ARevillaSearchFight.Models;
+using ARevillaSearchFight.Presenters;
+using ARevillaSearchFight.Views.Implementations;
 
 namespace ARevillaSearchFight
 {
@@ -9,6 +14,16 @@ namespace ARevillaSearchFight
     {
         public static void Main(string[] args)
         {
+            var services = new ServiceCollection();
+            services
+                .AddSingleton<ISearchFightView, SearchFightConsoleView>()
+                .AddSingleton<ISearchFightModel, Models.Implementations.SearchFightModel>()
+                .AddSingleton<ISearchEngine, Engines.GoogleCustomSearchEngine>()
+                .AddSingleton<ISearchEngine, Engines.BingSearchEngine>();
+            
+            var provider = services.BuildServiceProvider();
+
+            var presenter = provider.GetRequiredService<SearchFightPresenter>();
         }
     }
 }
