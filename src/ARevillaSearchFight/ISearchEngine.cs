0f1076a9f50acbe8440dab4b83bc820ddd3e1fb4
Alfredo Revilla <alfredorevilla@gmail.com>
2017-01-10 15:54:50 -0500

    Added paged search method to iface.

--- src/ARevillaSearchFight/ISearchEngine.cs
+++ src/ARevillaSearchFight/ISearchEngine.cs
@@ -12,16 +12,19 @@ namespace ARevillaSearchFight
     public interface ISearchEngine
     {
         /// <summary>
-        /// Engine identifier
-        /// </summary>
-        string Name { get; }
-
-        /// <summary>
         /// Search method
         /// </summary>
         /// <param name="term">The term to search for</param>
         /// <returns cref="SearchResults">A SearchResults object</returns>
         /// <exception cref="System.ArgumentNullException"></exception>
-        SearchResults Search(string term);
+        SearchResults Search(string term, int offset, int maxResults);
+
+
+        /// <summary>
+        /// Returns total number of search results found by the engine or service
+        /// </summary>
+        /// <param name="term"></param>
+        /// <returns></returns>
+        int GetSearchTotalCount(string term);
     }
 }
