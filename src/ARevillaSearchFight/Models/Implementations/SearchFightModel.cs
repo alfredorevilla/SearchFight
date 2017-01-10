f368c57d77b45a7f27d063a418fca66e9891d72f
Alfredo Revilla <alfredorevilla@gmail.com>
2017-01-10 15:15:11 -0500

    Added first model and view empty implementations.

---
+++ src/ARevillaSearchFight/Models/Implementations/SearchFightModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ARevillaSearchFight.Views.Models;
+
+namespace ARevillaSearchFight.Models.Implementations
+{
+    public class SearchFightModel : ISearchFightModel
+    {
+        public SearchFightModel(IEnumerable<ISearchEngine> engines)
+        {
+            this.Engines = engines;
+        }
+
+        public IEnumerable<ISearchEngine> Engines { get; }
+
+        public string GetOverallWinnerTerm(string[] terms)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultCountPerTermPerEngine[] GetResultsCountPerTermPerEngine(string[] terms)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WinnerTermPerEngine[] GetWinnersTermsPerSearchEngine(string[] terms)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateTerms(string[] terms, out string[] validationErrors)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ARevillaSearchFight/Views/Implementations/SearchFightConsoleView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ARevillaSearchFight.Views.Models;
+
+namespace ARevillaSearchFight.Views.Implementations
+{
+    public class SearchFightConsoleView : ISearchFightView
+    {
+        public event EventHandler<SearchAndFightArgs> SearchAndFight;
+
+        public void RenderError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderSearchAndFightData(SearchAndFightData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderWarningList(string titleOrCategory, string[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
